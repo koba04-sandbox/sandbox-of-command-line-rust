d40a105ad6a2d8c93a4a09f5ae9ac3c1d9a16fe4
Toru Kobayashi <koba0004@gmail.com>
2023-03-05 15:38:13 +0900

    wip: before Extracting Characters or Bytes

--- cutr/src/lib.rs
+++ cutr/src/lib.rs
@@ -1,6 +1,8 @@
 use crate::Extract::*;
 use clap::{App, Arg};
 use std::{error::Error, ops::Range};
+use std::num::NonZeroUsize;
+use regex::Regex;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 type PositionList = Vec<Range<usize>>;
@@ -12,8 +14,130 @@ pub enum Extract {
     Chars(PositionList)
 }
 
+#[derive(Debug)]
 pub struct Config {
     files: Vec<String>,
     delimiter: u8,
     extract: Extract
+}
+
+fn parse_pos(range: &str) -> MyResult<PositionList> {
+    let range_re = Regex::new(r"^(\d+)-(\d+)$").unwrap();
+    range
+        .split(',')
+        .into_iter()
+        .map(|val| {
+            parse_index(val).map(|n| n..n + 1).or_else(|e| {
+                range_re.captures(val).ok_or(e).and_then(|captures| {
+                    let n1= parse_index(&captures[1])?;
+                    let n2 = parse_index(&captures[2])?;
+                    if n1 >= n2 {
+                        return Err(format!(
+                            "First number in range ({}) \
+                            must be lower than second number ({})",
+                            n1 + 1,
+                            n2 + 2
+                        ));
+                    }
+                    Ok(n1..n2 + 1)
+                })
+            })
+        })
+        .collect::<Result<_, _>>()
+        .map_err(From::from)
+}
+
+fn parse_index(input: &str) -> Result<usize, String> {
+    let value_error = || format!("illigal list value: \"{}\"", input);
+    input
+        .starts_with("+")
+        .then(|| Err(value_error()))
+        .unwrap_or_else(|| {
+            input
+                .parse::<NonZeroUsize>()
+                .map(|n| usize::from(n) - 1)
+                .map_err(|_| value_error())
+        })
+
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("cutr")
+        .version("0.1.0")
+        .author("koba04")
+        .about("Rust cut")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-")
+        )
+        .arg(
+            Arg::with_name("delimiter")
+                .value_name("DELIMITER")
+                .short("d")
+                .long("delim")
+                .help("Field delimiter")
+                .default_value("\t")
+        )
+        .arg(
+            Arg::with_name("fields")
+                .value_name("FIELDS")
+                .short("f")
+                .long("fields")
+                .help("Selected fields")
+                .conflicts_with_all(&["chars", "bytes"])
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .value_name("BYTES")
+                .short("b")
+                .long("bytes")
+                .help("Selected bytes")
+                .conflicts_with_all(&["fields", "chars"])
+        )
+        .arg(
+            Arg::with_name("chars")
+                .value_name("CHARS")
+                .short("c")
+                .long("chars")
+                .help("Selected characters")
+                .conflicts_with_all(&["fields", "bytes"])
+        )
+        .get_matches();
+
+    let delimiter = matches.value_of("delimiter").unwrap();
+    let delim_bytes = delimiter.as_bytes();
+    if delim_bytes.len() != 1 {
+        return Err(From::from(format!(
+            "--delim \"{}\" must be a single byte",
+            delimiter
+        )));
+    }
+
+    let fields = matches.value_of("fields").map(parse_pos).transpose()?;
+    let bytes = matches.value_of("bytes").map(parse_pos).transpose()?;
+    let chars = matches.value_of("chars").map(parse_pos).transpose()?;
+
+    let extract = if let Some(field_pos) = fields {
+        Fields(field_pos)
+    } else if let Some(bytes_pos) = bytes {
+        Bytes(bytes_pos)
+    } else if let Some(char_pos) = chars {
+        Chars(char_pos)
+    } else {
+        return Err(From::from("Must have --fields, --bytes, or --chars"))
+    };
+
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        delimiter: *delim_bytes.first().unwrap(),
+        extract,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", &config);
+    Ok(())
 }
